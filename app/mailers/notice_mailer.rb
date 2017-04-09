1ae0682e1d07699de94c8ab14a59512ef3873b2b
ikedaCoffee <kouhik06@gmail.com>
2017-04-09 07:41:25 +0000

    Update DIVE11

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -11,4 +11,11 @@ class NoticeMailer < ApplicationMailer
     mail to: "kouhik06@gmail.com",
       subject: '【Achieve】ブログが投稿されました'
   end
+
+  def sendmail_contact(contact)
+    @contact = contact
+
+    mail to: @contact.email,
+      subject: '【Achieve】お問い合わせありがとうございました'
+  end
 end
