42983603b5088dffd32bd39a945d454a84332fcc
ikedaCoffee <kouhik06@gmail.com>
2017-04-11 11:18:28 +0000

    Update DIVE11

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -8,7 +8,7 @@ class NoticeMailer < ApplicationMailer
   def sendmail_blog(blog)
     @blog = blog
 
-    mail to: "kouhik06@gmail.com",
+    mail to: @blog.user.email,
       subject: '【Achieve】ブログが投稿されました'
   end
 
